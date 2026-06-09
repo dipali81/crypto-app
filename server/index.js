ae79ec47a0e6acc0e988bd3799836add7ed9166b
dipali81 <boradedipali81@gmail.com>
2026-06-09 22:03:41 +0530

    [feat] : developed login, register apis

--- server/index.js
+++ server/index.js
@@ -2,17 +2,22 @@ import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import dns from "node:dns/promises";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config(); // loads .env variables
 dns.setServers(["1.1.1.1"]);
-const app = express(); 
+const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("get route")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("server started http://localhost:5000/")
 })
